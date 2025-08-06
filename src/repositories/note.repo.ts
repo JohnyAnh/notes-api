11d7d864780a105ba54bf686b7d0dfc0d24c6716
JohnyVu <johnyanhn01@gmail.com>
2025-08-06 12:54:09 +0700

    feat: add NoteRepo.create()

--- src/repositories/note.repo.ts
+++ src/repositories/note.repo.ts
@@ -1,3 +1,18 @@
 import { openDb } from './db';
 import { Note } from '../models/note';
 import { v4 as uuid } from 'uuid';
+
+export class NoteRepo{
+    async create(data: Omit<Note, 'id' | 'createAt' | 'updateAt'>): Promise<Note> {
+        const db = await openDb();
+        const id = uuid();
+        const now = new Date().toISOString();
+        await db.run(`
+            INSERT INTO notes (id, titel, body, tags, createAt, updateAt)
+            VALUES (?, ?, ?, ?, ?, ?)`,
+            id, data.titel, data.body, JSON.stringify(data.tags), now, now
+        );
+        return {id, ...data, createAt: now, updateAt: now};
+    }
+    //TODO: list, getById, update, delete methods
+}
